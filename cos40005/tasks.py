0d9e2f1bbe966a44b099cc5501339466165fdc05
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-06-18 23:05:19 +0700

    Update gitignore

--- cos40005/tasks.py
+++ cos40005/tasks.py
@@ -3,22 +3,22 @@ from .models import Property, Domain
 
 @shared_task
 def insert_data_to_db(data):
-    for items in data:
-        domain, created = Domain.objects.get_or_create(title=items['domain'])
-        property_data = Property(
-            domain=domain,
-            title=items['title'],
-            address=items['address'],
-            price=items['price'],
-            area=items['area'],
-            floor=items['floor'],
-            bedroom=items['bedroom'],
-            toilet=items['toilet'],
-            publish_date=items['publish_date'],
-            contact=items['contact'],
-            description=items['description']
-        )
-        property_data.save()
+    print(data)
+    domain, created = Domain.objects.get_or_create(title=data['domain'])
+    property_data = Property(
+        domain=domain,
+        title=data['title'],
+        address=data['address'],
+        price=data['price'],
+        area=data['area'],
+        floor=data['floor'],
+        bedroom=data['bedroom'],
+        toilet=data['toilet'],
+        publish_date=data['publish_date'],
+        contact=data['contact'],
+        description=data['description']
+    )
+    property_data.save()
 
 def crawl_data():
     print()
